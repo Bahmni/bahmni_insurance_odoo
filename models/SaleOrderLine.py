745ea2e488d2a7baa42f23d4744f0d97f220bfab
laxmanm7 <laxmanmanandhar7@gmail.com>
2019-09-16 15:18:10 +0545

    Revert "Bahimis 206"

---
+++ models/SaleOrderLine.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class SaleOrderLine(models.Model):
+    _name = 'sale.order.line'
+    _inherit = 'sale.order.line'
+
+    payment_type = fields.Selection([('insurance', 'INSURANCE'), ('cash', 'CASH')], default='cash', string="Payment Type", required=True)
+
+SaleOrderLine()
--- wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-import insurance_eligibility
